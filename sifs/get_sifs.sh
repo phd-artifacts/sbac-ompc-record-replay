d2e9e39a0aa8786b46c1de08489838712db4dac9
Rodrigo Ceccato <rodrigoceccatodefreitas@gmail.com>
2025-06-12 09:46:42 -0400

    update branchs and sim

--- sifs/get_sifs.sh
+++
@@ -1,5 +0,0 @@
-apptainer pull docker://registry.gitlab.com/ompcluster/containers/build-runtime-img/ubuntu20.04-cuda11.2-mpich:1854063649
-apptainer pull docker://registry.gitlab.com/ompcluster/containers/build-runtime-img/ubuntu20.04-cuda11.2-mpich:1854061961
-
-mv ubuntu20.04-cuda11.2-mpich_1854063649.sif scheduler-main.sif
-mv ubuntu20.04-cuda11.2-mpich_1854061961.sif improv-144.sif
